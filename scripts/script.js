d671d30a9c9800dc88a6a083c9f1db04ddd76200
Gonzalo Matias  Lafuente <lafuentegonzalo1@gmail.com>
2024-01-12 20:30:25 -0300

    agrego funcion de selector de grid, elliminacion de grid y contruccion

--- scripts/script.js
+++ scripts/script.js
@@ -23,19 +23,18 @@ function createGrid(total_squares){
 }
 
 function deleteGrid(){
-    for (let index = 0; index<total_squares*total_squares; index++) {
-        let sketch_square = document.createElement("div");
-        sketch_square.setAttribute("class","square");
-        sketch_square.setAttribute("style","width: "+square_size+"px;height: "+square_size+"px; background-color:black ;flex-shrink: 4; color: white;")
-        sketch_square.textContent = "10";
-        grid.append(sketch_square);
-    }
+    const squares_grid = document.querySelectorAll(".square");
+    squares_grid.forEach(square=> {
+        grid.removeChild(square);
+    });
 }
 
 function addDragMotion(){
     range_value.textContent = range_input.value;
     range_input.addEventListener("input",(e)=>{
         range_value.textContent = e.target.value;
+        deleteGrid();
+        createGrid(e.target.value);
     });
 }
 
